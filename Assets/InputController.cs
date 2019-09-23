fa3715db3b87538f73efd42e58879d0ecff9b910
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-23 11:12:38 +0200

    implemented swipe controls

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -7,6 +7,7 @@ public class InputController : MonoBehaviour
     [SerializeField]
     private float minSwipeDistance = 0.10f;
     private float swipeDistance;
+    private bool hasSwiped;
 
     private bool trackMouse;
 
@@ -29,6 +30,9 @@ public class InputController : MonoBehaviour
 #endif
     }
 
+    /// <summary>
+    /// Handles mobile input
+    /// </summary>
     private void MobileInput()
     {
         if (Input.touchCount > 0)
@@ -43,35 +47,70 @@ public class InputController : MonoBehaviour
             {
                 lastPosition = touch.position;
                 Vector3 direction = lastPosition - firstPosition;
-                if (direction.magnitude > swipeDistance)
+                if (direction.magnitude > swipeDistance && !hasSwiped)
                 {
-                    Debug.Log(direction.normalized);
+                    // swiped left side of the screen
+                    if (firstPosition.x < Screen.width / 2f)
+                    {
+                        // do something with direction.normalized
+                        hasSwiped = true;
+                    }
+                    // swiped right side of the screen
+                    else
+                    {
+                        // do something with direction.normalized
+                        hasSwiped = true;
+                    }
                 }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                hasSwiped = false;
             }
         }
     }
 
+    /// <summary>
+    /// Handles pc input
+    /// </summary>
     private void PcInput()
     {
         if (Input.GetMouseButtonDown(0))
         {
             trackMouse = true;
             firstPosition = Input.mousePosition;
-            lastPosition = Input.mousePosition;
+            lastPosition = Input.mousePosition;         
         }
 
         if (Input.GetMouseButtonUp(0))
+        {
             trackMouse = false;
+            hasSwiped = false;
+        }
 
-        if (trackMouse)
+        if (trackMouse && !hasSwiped)
         {
             lastPosition = Input.mousePosition;
             Vector3 direction = lastPosition - firstPosition;
             if (direction.magnitude > swipeDistance)
             {
-                Debug.Log(direction.normalized);
+                // swiped left side of the screen
+                if (firstPosition.x < Screen.width / 2f)
+                {
+                    Debug.Log("Swiped Left Side");
+                    // do something with direction.normalized
+                    Debug.Log(direction.normalized);
+                    hasSwiped = true;
+                }
+                // swiped right side of the screen
+                else
+                {
+                    Debug.Log("Swiped Right Side");
+                    // do something with direction.normalized
+                    Debug.Log(direction.normalized);
+                    hasSwiped = true;
+                }
             }
         }
     }
-
 }
