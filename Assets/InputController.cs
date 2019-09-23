3f462bcc0528d5f12f6467ce6bf3ce801209e251
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-23 12:57:30 +0200

    finished input for one touch

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -1,37 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
     private Vector3 firstPosition;
     private Vector3 lastPosition;
     [SerializeField]
-    private float minSwipeDistance = 0.10f;
+    private float minSwipeDistanceInPercentage = 0.10f;
     private float swipeDistance;
     private bool hasSwiped;
 
     private bool trackMouse;
 
+    public enum SwipeDirection { Up, Down, Left, Right }
+
+    public SwipeDirection swipeDirection { get; private set; }
+
     private void Start()
     {
-        swipeDistance = Screen.height * minSwipeDistance;
+        swipeDistance = Screen.height * minSwipeDistanceInPercentage;
     }
 
+    /// <summary>
+    /// Update function.
+    /// </summary>
     public void Tick()
     {
         HandleInput();
     }
 
+    /// <summary>
+    /// Handles the input.
+    /// </summary>
     private void HandleInput()
     {
-#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+#if UNITY_EDITOR || UNITY_STANDALONE
         PcInput();
-#elif UNITY_ANDROID
+#elif UNITY_ANDROID || UNITY_IOS
         MobileInput();
 #endif
     }
 
     /// <summary>
-    /// Handles mobile input
+    /// Handles mobile input.
     /// </summary>
     private void MobileInput()
     {
@@ -46,22 +57,7 @@ public class InputController : MonoBehaviour
             else if (touch.phase == TouchPhase.Moved)
             {
                 lastPosition = touch.position;
-                Vector3 direction = lastPosition - firstPosition;
-                if (direction.magnitude > swipeDistance && !hasSwiped)
-                {
-                    // swiped left side of the screen
-                    if (firstPosition.x < Screen.width / 2f)
-                    {
-                        // do something with direction.normalized
-                        hasSwiped = true;
-                    }
-                    // swiped right side of the screen
-                    else
-                    {
-                        // do something with direction.normalized
-                        hasSwiped = true;
-                    }
-                }
+                CheckSwipe();
             }
             else if (touch.phase == TouchPhase.Ended)
             {
@@ -71,7 +67,7 @@ public class InputController : MonoBehaviour
     }
 
     /// <summary>
-    /// Handles pc input
+    /// Handles pc input.
     /// </summary>
     private void PcInput()
     {
@@ -91,26 +87,117 @@ public class InputController : MonoBehaviour
         if (trackMouse && !hasSwiped)
         {
             lastPosition = Input.mousePosition;
-            Vector3 direction = lastPosition - firstPosition;
-            if (direction.magnitude > swipeDistance)
+            CheckSwipe();
+        }
+    }
+
+    /// <summary>
+    /// Checks how to player has swiped.
+    /// </summary>
+    private void CheckSwipe()
+    {
+        Vector3 direction = lastPosition - firstPosition;
+
+        // check if the player has swiped enough
+        if (!(Math.Abs(direction.x) > swipeDistance) &&
+            !(Math.Abs(direction.y) > swipeDistance)) return;
+
+        // horizontal swipe
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            // swiped to right
+            if (lastPosition.x > firstPosition.x)
             {
-                // swiped left side of the screen
-                if (firstPosition.x < Screen.width / 2f)
-                {
-                    Debug.Log("Swiped Left Side");
-                    // do something with direction.normalized
-                    Debug.Log(direction.normalized);
-                    hasSwiped = true;
-                }
-                // swiped right side of the screen
-                else
-                {
-                    Debug.Log("Swiped Right Side");
-                    // do something with direction.normalized
-                    Debug.Log(direction.normalized);
-                    hasSwiped = true;
-                }
+                swipeDirection = SwipeDirection.Right;
+                SwipeLocation();
             }
+            // swiped to left
+            else
+            {
+                swipeDirection = SwipeDirection.Left;
+                SwipeLocation();
+            }
+        }
+        // vertical swipe
+        else
+        {
+            // swipe up
+            if (lastPosition.y > firstPosition.y)
+            {
+                swipeDirection = SwipeDirection.Up;
+                SwipeLocation();
+            }
+            // swipe down
+            else
+            {
+                swipeDirection = SwipeDirection.Down;
+                SwipeLocation();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Determines swipe location.
+    /// </summary>
+    private void SwipeLocation()
+    {
+        // swiped left side of the screen
+        if (firstPosition.x < Screen.width / 2f)
+        {
+            hasSwiped = true;
+            SwipeLeftScreen();
+        }
+        // swiped right side of the screen
+        else
+        {
+            hasSwiped = true;
+            SwipeRightScreen();
+        }
+    }
+
+    // todo move to another class that handles animation and logic
+    private void SwipeLeftScreen()
+    {
+        Debug.Log("Swiped Left Side");
+        switch (swipeDirection)
+        {
+            case SwipeDirection.Down:
+                Debug.Log("Swiped to down");
+                // todo play right hand swipe down animation
+                break;
+            case SwipeDirection.Up:
+                Debug.Log("Swiped to up");
+                // todo play right hand swipe up animation
+                break;
+            case SwipeDirection.Right:
+                Debug.Log("Swiped to right");
+                // todo play right hand swipe to right animation
+                break;
+            default:
+                break;
+        }
+    }
+
+    // todo move to another class that handles animation and logic
+    private void SwipeRightScreen()
+    {
+        Debug.Log("Swiped Right Side");
+        switch (swipeDirection)
+        {
+            case SwipeDirection.Down:
+                Debug.Log("Swiped to down");
+                // todo play left hand swipe down animation
+                break;
+            case SwipeDirection.Up:
+                Debug.Log("Swiped to up");
+                // todo play left hand swipe up animation
+                break;
+            case SwipeDirection.Left:
+                Debug.Log("Swiped to left");
+                // todo play left hand swipe to left side animation
+                break;
+            default:
+                break;
         }
     }
 }
