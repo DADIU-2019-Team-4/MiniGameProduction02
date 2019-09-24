ceb0fc0f316a6de4ba22e7d17ac1292f6b60d049
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-24 10:43:03 +0200

    started on progression

---
+++ Assets/Scripts/ProgressionController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressionController : MonoBehaviour
+{
+    [SerializeField]
+    private Image filling;
+
+    [SerializeField]
+    private float startAmount = 0.5f;
+
+    [SerializeField]
+    private float depleteSpeed = 0.1f;
+
+    private SceneController sceneController;
+
+    private void Awake()
+    {
+        sceneController = FindObjectOfType<SceneController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filling.fillAmount = startAmount;
+    }
+
+    private void Update()
+    {
+        filling.fillAmount -= Time.deltaTime * depleteSpeed;
+
+        if (filling.fillAmount <= 0)
+            sceneController.LevelFailed();
+
+    }
+
+    public void UpdateProgression()
+    {
+
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,8 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject failText;
+
     public void SceneReset()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LevelFailed()
+    {
+        failText.SetActive(true);
     }
 }
