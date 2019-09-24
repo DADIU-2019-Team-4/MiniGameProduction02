3399b43204c39d3ca2b31c6bbebda59327a8d678
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-24 12:00:10 +0200

    added timer

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
     [SerializeField]
     private GameObject failText;
+
+    [SerializeField]
+    private Text timerText;
+
+    [SerializeField]
+    private float timerValue = 60f;
+
+    private void Update()
+    {
+        timerValue -= Time.deltaTime;
+
+        string minutes = Mathf.Floor(timerValue / 60).ToString("0");
+        string seconds = (timerValue % 60).ToString("00");
+
+        timerText.text = $"{minutes}:{seconds}";
+    }
 
     public void SceneReset()
     {
