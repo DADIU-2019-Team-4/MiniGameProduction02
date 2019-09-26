d863bbc5214b06642728c1f2ac02963dc1f84e3f
Joakim Carlsen <joakim.carlsen@hotmail.com>
2019-09-26 15:32:48 +0200

    Blocker and Collector prototypes

---
+++ Assets/CollectionPrototype/CollectionItemSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionItemSpawner : MonoBehaviour
+{
+    public float minTime = 2f;
+    public float maxTime = 8f;
+
+    public GameObject spawnPosTop;
+    public GameObject spawnPosMid;
+    public GameObject spawnPosBottom;
+
+    public GameObject prefab;
+
+    public bool isTop = false;
+    public bool isMid = false;
+    public bool isBottom = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTop == false)
+        {
+            StartCoroutine(randomSpawn(Random.Range(minTime, maxTime), spawnPosTop, "top"));
+            isTop = true;
+        }
+
+        if (isMid == false)
+        {
+            StartCoroutine(randomSpawn(Random.Range(minTime, maxTime), spawnPosMid, "mid"));
+            isMid = true;
+        }
+
+        if (isBottom == false)
+        {
+            StartCoroutine(randomSpawn(Random.Range(minTime, maxTime), spawnPosBottom, "bottom"));
+            isBottom = true;
+        }
+    }
+
+    private IEnumerator randomSpawn(float _seconds, GameObject _spawnPos, string _placement)
+    {
+        yield return new WaitForSeconds(_seconds);
+
+
+        GameObject item = Instantiate(prefab, _spawnPos.transform.position, Quaternion.identity);
+        item.transform.position = _spawnPos.transform.position;
+        item.gameObject.GetComponent<CollectionItem>().placement = _placement;
+        print("spawned top");
+    }
+}
