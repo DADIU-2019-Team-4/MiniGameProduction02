b74ac07ca052059eef346c1853b8b2fb9efceb2e
magei00 <magnuseilersen@yahoo.dk>
2019-09-26 13:55:37 +0200

    added Balloon prefab

---
+++ Assets/Scripts/Controllers/ScriptableJuggleObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/JuggleObject", order = 1)]
+public class ScriptableJuggleObject : ScriptableObject
+{
+    public string objectName;
+    public Mesh objectMesh;
+    public float mass;
+    public bool willStickToFloor;
+
+}
