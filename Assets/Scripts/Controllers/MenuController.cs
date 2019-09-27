1a2a40472da226e5890f314dd075424386a49b9a
magei00 <magnuseilersen@yahoo.dk>
2019-09-27 16:42:42 +0200

    Menu navigation is starting to work. Still need to make options do something.

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    private Canvas pauseMenu;
+    private Canvas levelSelectMenu;
     public SaveController saveC;
+    public GameObject pauseMenu;
+    public GameObject optionsMenu;
+    public GameObject resetMenu;
 
     //test variables
     public int levelSelector = 0; 
@@ -15,14 +18,21 @@ public class MenuController : MonoBehaviour
     private void Awake()
     {
         saveC = FindObjectOfType<SaveController>();
-        pauseMenu = GameObject.FindGameObjectWithTag("pauseMenu").GetComponent<Canvas>();
-        Debug.Log(pauseMenu.gameObject.name);
+        //levelSelectMenu = GameObject.FindGameObjectWithTag("pauseMenu").GetComponent<Canvas>();
+        //Debug.Log(levelSelectMenu.gameObject.name);
+        resetMenu = GameObject.Find("ResetGameMenu").gameObject;
+        resetMenu.SetActive(false);
+        optionsMenu = GameObject.Find("Options").gameObject;
+        optionsMenu.SetActive(false);
+        pauseMenu = GameObject.Find("PauseMenu").gameObject;
+        pauseMenu.SetActive(false);
+        
+
     }
 
     void Start()
     {
-        Button[] levelbuttons = pauseMenu.transform.GetChild(0).GetComponentsInChildren<Button>();
-
+        Button[] levelbuttons = levelSelectMenu.transform.GetChild(0).GetComponentsInChildren<Button>();
         foreach (Button button in levelbuttons)
         {
             button.interactable = false;
@@ -38,7 +48,7 @@ public class MenuController : MonoBehaviour
 
     public void UpdatePauseMenu()
     {
-        Button[] levelbuttons = pauseMenu.transform.GetChild(0).GetComponentsInChildren<Button>();
+        Button[] levelbuttons = levelSelectMenu.transform.GetChild(0).GetComponentsInChildren<Button>();
 
         for (int i=0; i < saveC.save.maxReachedLevel; i++)
         {
@@ -46,4 +56,86 @@ public class MenuController : MonoBehaviour
             Debug.Log(saveC.save.maxReachedLevel);
         }
     }
+
+    public void GoToLevelSelectScene()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void ResetLevel()
+    {
+        //TODO
+    }
+
+    public void SwitchLanguageToDanish()
+    {
+
+    }
+
+    public void SwitchLanguageToEnglish()
+    {
+
+    }
+
+    public void MusicChangeToLow()
+    {
+
+    }
+
+    public void MusicChangeToHigh()
+    {
+
+    }
+
+    public void MusicChangeToOff()
+    {
+
+    }
+
+    public void SoundTurnOff()
+    {
+
+    }
+
+    public void SoundTurnOn()
+    {
+
+    }
+
+    public void GoToRestartMenu()
+    {
+
+        resetMenu.SetActive(true);
+    }
+
+    public void BackToGame()
+    {
+        pauseMenu.SetActive(false);
+
+        optionsMenu.SetActive(false);
+
+
+        resetMenu.SetActive(false);
+    }
+
+    public void GoToPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+
+        optionsMenu.SetActive(false);
+    }
+
+    public void GoToOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
+
+
+        resetMenu.SetActive(false);
+    }
+
+    public void RestartGameProgress()
+    {
+        Debug.Log("Game Progress is restarted");
+    }
+
 }
