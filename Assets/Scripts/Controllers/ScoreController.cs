62da3e99afce186f8d3370ca8c831a00b85cf396
MichaeliusDenModige <s143049@student.dtu.dk>
2019-09-30 11:05:38 +0200

    Cleaned up merge

---
+++ Assets/Scripts/Controllers/ScoreController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    private SceneController SceneController;
+
+    public Text scoreText;
+    [SerializeField]
+    private int score;
+    private int receivedPoints;
+    [SerializeField]
+    private int itemHitCombo = 0;
+
+    public enum CatchType { Normal, Perfect, Failed }
+
+    private void Awake()
+    {
+        SceneController = FindObjectOfType<SceneController>();
+    }
+
+    public void IncrementScore(bool wasPerfectlyThrown)
+    {
+        itemHitCombo++;
+        if (!wasPerfectlyThrown)
+            score += itemHitCombo;
+        else
+        {
+            // Code for perfect throws. Not entirely decided on yet.
+            score += itemHitCombo * 2;
+        }
+        scoreText.text = "Score: " + score;
+    }
+
+    public void ResetMultiplier()
+    {
+        itemHitCombo = 0;
+    }
+
+}
