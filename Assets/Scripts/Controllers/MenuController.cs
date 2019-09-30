2e325681580b9c70951ff23a813dcaef96911319
magei00 <magnuseilersen@yahoo.dk>
2019-09-30 11:14:32 +0200

    Menu navigation fully done, and settings are now saved in Playerprefs

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -11,12 +11,40 @@ public class MenuController : MonoBehaviour
     public GameObject pauseMenu;
     public GameObject optionsMenu;
     public GameObject resetMenu;
+    public string language;
+    public string musicVolume;
+    public string soundOnOff;
 
     //test variables
-    public int levelSelector = 0; 
+    public int levelSelector = 0;
+
+
+    #region menuvariables
+    private GameObject settingsTextImage;
+    private GameObject indstillingerTextImage;
+
+    private Text[] soundOnOffContainer;
+    private Text[] musicContainer;
+
+    private GameObject englishButton;
+    private GameObject danishButton;
+    private GameObject restartButton;
+
+    private GameObject musicHigh;
+    private GameObject musicLow;
+    private GameObject musicOff;
+
+    private GameObject soundOn;
+    private GameObject soundOff;
+    
+
+    #endregion
 
     private void Awake()
     {
+
+        RefferenceMenuVariables();
+        MenuUpdate(); //setup menu
         saveC = FindObjectOfType<SaveController>();
         //levelSelectMenu = GameObject.FindGameObjectWithTag("pauseMenu").GetComponent<Canvas>();
         //Debug.Log(levelSelectMenu.gameObject.name);
@@ -26,7 +54,9 @@ public class MenuController : MonoBehaviour
         optionsMenu.SetActive(false);
         pauseMenu = GameObject.Find("PauseMenu").gameObject;
         pauseMenu.SetActive(false);
-        
+
+
+
 
     }
 
@@ -50,7 +80,7 @@ public class MenuController : MonoBehaviour
     {
         Button[] levelbuttons = levelSelectMenu.transform.GetChild(0).GetComponentsInChildren<Button>();
 
-        for (int i=0; i < saveC.save.maxReachedLevel; i++)
+        for (int i = 0; i < saveC.save.maxReachedLevel; i++)
         {
             levelbuttons[i].interactable = true;
             Debug.Log(saveC.save.maxReachedLevel);
@@ -70,36 +100,51 @@ public class MenuController : MonoBehaviour
     public void SwitchLanguageToDanish()
     {
 
+        PlayerPrefs.SetString("Lang", "Danish");
+        Debug.Log("Changed Lang to danish");
+        MenuUpdate();
     }
 
     public void SwitchLanguageToEnglish()
     {
+        PlayerPrefs.SetString("Lang", "English");
 
+        Debug.Log("Changed Lang to English");
+        MenuUpdate();
     }
 
     public void MusicChangeToLow()
     {
-        
+        PlayerPrefs.SetString("MusVol", "Low");
+        MenuUpdate();
     }
 
     public void MusicChangeToHigh()
     {
 
+        PlayerPrefs.SetString("MusVol", "High");
+        MenuUpdate();
     }
 
     public void MusicChangeToOff()
     {
 
+        PlayerPrefs.SetString("MusVol", "Off");
+        MenuUpdate();
     }
 
     public void SoundTurnOff()
     {
 
+        PlayerPrefs.SetString("SoundOnOff", "Off");
+        MenuUpdate();
     }
 
     public void SoundTurnOn()
     {
 
+        PlayerPrefs.SetString("SoundOnOff", "On");
+        MenuUpdate();
     }
 
     public void GoToRestartMenu()
@@ -128,14 +173,160 @@ public class MenuController : MonoBehaviour
     public void GoToOptionsMenu()
     {
         optionsMenu.SetActive(true);
-
-
         resetMenu.SetActive(false);
     }
 
     public void RestartGameProgress()
     {
         Debug.Log("Game Progress is restarted");
+        GoToOptionsMenu();
+    }
+
+    public void UpdateLanguageTextToDanish()
+    {
+
+    }
+
+    public void UpdateLanguageTextToEnglísh()
+    {
+
+    }
+
+    private void MenuUpdate()
+    {
+        //setup all the playerprefs the first time the player plays
+
+        if (!PlayerPrefs.HasKey("Lang"))
+        {
+            PlayerPrefs.SetString("Lang", "Eng");
+        }
+
+        if (!PlayerPrefs.HasKey("MusVol"))
+        {
+            PlayerPrefs.SetString("MusVol", "High");
+        }
+
+        if (!PlayerPrefs.HasKey("SoundOnOff"))
+        {
+            PlayerPrefs.SetString("SoundOnOff", "On");
+        }
+
+        //Show correct information in menu
+
+        language = PlayerPrefs.GetString("Lang");
+        musicVolume = PlayerPrefs.GetString("MusVol");
+        soundOnOff = PlayerPrefs.GetString("SoundOnOff");
+
+        //||set language text---------------------------------------------------------
+
+        //gameobject variables
+
+        
+
+        if (language == "English")
+        {
+            settingsTextImage.SetActive(true);
+            indstillingerTextImage.SetActive(false);
+
+            foreach (Text text in soundOnOffContainer)
+            {
+                text.text = "Sound";
+            }
+
+            foreach (Text text in musicContainer)
+            {
+                text.text = "Volume";
+            }
+
+            englishButton.SetActive(true);
+            danishButton.SetActive(false);
+
+            restartButton.GetComponentInChildren<Text>().text = "Reset Game";
+
+        }
+        else if (language == "Danish")
+        {
+            settingsTextImage.SetActive(false);
+            indstillingerTextImage.SetActive(true);
+
+            foreach (Text text in soundOnOffContainer)
+            {
+                text.text = "Lyd";
+            }
+
+            foreach (Text text in musicContainer)
+            {
+                text.text = "Lydstyrke";
+            }
+
+            englishButton.SetActive(false);
+            danishButton.SetActive(true);
+
+            restartButton.GetComponentInChildren<Text>().text = "Slet Fremskridt";
+        }
+        else
+        {
+            Debug.Log("language in menucontroller has incorrect value: "+ language);
+        }
+
+        //Volume
+        if (musicVolume == "High")
+        {
+            musicHigh.SetActive(true);
+            musicLow.SetActive(false);
+            musicOff.SetActive(false);
+
+        }
+        else if (musicVolume == "Low")
+        {
+            musicHigh.SetActive(false);
+            musicLow.SetActive(true);
+            musicOff.SetActive(false);
+        }
+        else if (musicVolume == "Off")
+        {
+            musicHigh.SetActive(false);
+            musicLow.SetActive(false);
+            musicOff.SetActive(true);
+        }
+
+
+        //SoundOnOff
+        if (soundOnOff == "On")
+        {
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+
+        }
+        else if (soundOnOff == "Off")
+        {
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+        }
+
+    }
+
+    public void RefferenceMenuVariables()
+    {
+        settingsTextImage = GameObject.Find("Settings text image");
+        indstillingerTextImage = GameObject.Find("Indstillinger text image");
+
+        soundOnOffContainer = GameObject.Find("SoundOnOffContainer").gameObject.GetComponentsInChildren<Text>();
+        musicContainer = GameObject.Find("MusicSettingscontainer").gameObject.GetComponentsInChildren<Text>();
+
+        musicHigh = GameObject.Find("MusicHighButton").gameObject;
+        musicLow = GameObject.Find("MusicLowButton").gameObject;
+        musicOff = GameObject.Find("MusicOffButton").gameObject;
+
+        soundOn = GameObject.Find("SoundOnButton").gameObject;
+        soundOff = GameObject.Find("SoundOffButton").gameObject;
+
+
+        englishButton = GameObject.Find("LanguageEnglishButton").gameObject;
+        danishButton = GameObject.Find("LanguageDanishButton").gameObject;
+        restartButton = GameObject.Find("ResetGameButton").gameObject;
     }
 
 }
+
+
