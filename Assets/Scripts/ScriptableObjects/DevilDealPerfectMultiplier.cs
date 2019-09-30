5837a9080a393369f4a97e59cb4fba5e8ba8be7f
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-09-30 11:49:05 +0200

    implemented devil deal: you can only build up score multiplier when perfect thrown

---
+++ Assets/Scripts/ScriptableObjects/DevilDealPerfectMultiplier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Devil Deal Perfect Multiplier", menuName = "ScriptableObjects/DevilDealPerfectMultiplier")]
+public class DevilDealPerfectMultiplier : DevilDeal
+{
+    public override void ApplyDevilDeal()
+    {
+        ScoreController scoreController = FindObjectOfType<ScoreController>();
+        scoreController.MultiplierDevilDealActivated = true;
+    }
+}
