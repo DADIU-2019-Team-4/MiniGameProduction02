77cf76b19cf40b360eac4fc3654f83b76cdfef36
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-10-01 10:14:58 +0200

    Added three devil deals: ball speed, throw ring size and cut off hand (disable one side of the screen)

---
+++ Assets/Scripts/ScriptableObjects/DevilDealBallSpeed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Devil Deal Ball Speed", menuName = "ScriptableObjects/DevilDealBallSpeed")]
+public class DevilDealBallSpeed : DevilDeal
+{
+    public float speedMultiplier;
+
+    public override void ApplyDevilDeal()
+    {
+        // using time scale for now
+        BallController ballController = FindObjectOfType<BallController>();
+        ballController.TimeScale *= speedMultiplier;
+    }
+}
